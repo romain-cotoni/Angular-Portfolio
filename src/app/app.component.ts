f1976671569584ab25ee5ebc79bfaf2b1fe46f3b
romain-cotoni <romain.cotoni@gmail.com>
2023-03-17 22:02:14 +0100

    add header + contact form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Portfolio';
+  showHeader = true;
+
+  constructor(private router: Router) {
+    this.router = router;
+  }
+
+  isShowHeader() { 
+    console.log(this.router.url);
+    if (this.router.url === '/login') {
+      this.showHeader = false;
+    } else {
+      this.showHeader = true;
+    }
+  }
 }
