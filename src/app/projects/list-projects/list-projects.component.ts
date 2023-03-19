466788c521ecc37507948fec84b8fc3214b6a9c5
romain-cotoni <romain.cotoni@gmail.com>
2023-03-19 19:07:16 +0100

    add projects cards

--- src/app/projects/list-projects/list-projects.component.ts
+++ src/app/projects/list-projects/list-projects.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-list-projects',
   templateUrl: './list-projects.component.html',
   styleUrls: ['./list-projects.component.css']
 })
 export class ListProjectsComponent {
+  imagePath="assets/icons/conception.png"
+  github="https://github.com/romain-cotoni/Angular-Portfolio"
 
 }
