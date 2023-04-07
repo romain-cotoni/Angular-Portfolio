d48af5fcd6c8a2f7d8eede2da3b76e9740f02588
romain-cotoni <romain.cotoni@gmail.com>
2023-04-07 23:06:45 +0200

    collapse collapseAll

--- src/app/personal/cv/cv.component.ts
+++ src/app/personal/cv/cv.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class CvComponent {
 
+  showAll = false;
+
+  map = new Map<string, boolean>([
+    ['airfrance', false],
+    ['infotel', false],
+    ['gns', false],
+    ['afpa-cda', false],
+    ['afpa-dwwm', false],
+    ['anglais', false],
+    ['espagnol', false],
+  ]);
+
+  collapse(value: string) {
+    this.map.set(value, !this.map.get(value));
+  }
+
+  collapseAll() {
+    this.showAll = !this.showAll;
+    this.map.forEach((_,key) => { this.map.set(key, this.showAll) })
+  }
+
+
 }
